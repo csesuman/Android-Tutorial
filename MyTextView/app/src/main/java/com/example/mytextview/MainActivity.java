86b31ce4782c867bca1926249e619f4b95eb9477
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-01-22 01:24:35 +0600

    Text View Find By Id (#5)
    Close #4

--- MyTextView/app/src/main/java/com/example/mytextview/MainActivity.java
+++ MyTextView/app/src/main/java/com/example/mytextview/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mytextview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
+    private TextView ageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameTextView = findViewById(R.id.nameTextViewId); // Type cass not needed now
+
+        ageTextView = findViewById(R.id.ageTextViewId);
+
+        nameTextView.setText("Another suman vaiya");
+        ageTextView.setText("25 years old");
+
     }
 }
