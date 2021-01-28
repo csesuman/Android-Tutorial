a3869e280a2a137c4b183208e7a8f9c0ab0fdbb7
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-01-28 13:29:47 +0600

    Fragment Layout
    fix #20

---
+++ FragmentLayout/app/src/main/java/com/example/fragmentlayout/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.fragmentlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runFragTxn(R.id.frameContainer, ImageFragment.getInstance(R.drawable.swift));
+        runFragTxn(R.id.frameBottom, BottomFragment.getInstance());
+    }
+
+//    private void runFragTxn() {
+//        FragmentManager manager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+////        fragmentTransaction.add(R.id.frameContainer, MyFragment.getInstance("My Data"));
+//        fragmentTransaction.replace(R.id.frameContainer, MyFragment.getInstance("My is My Code"));
+//        fragmentTransaction.commit();
+//    }
+
+    public void runFragTxn(int container, Fragment fragment) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        fragmentTransaction.replace( container, fragment);
+        fragmentTransaction.commit();
+    }
+}
