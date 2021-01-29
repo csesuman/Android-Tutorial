7ecca5c23de821cdd35d635e63108622aaabd96b
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-01-29 20:15:11 +0600

    queryBroadcastReceivers not working.. so far good

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.java
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/ExampleBroadcastReceiver.java
@@ -10,25 +10,7 @@ import android.widget.Toast;
 public class ExampleBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-         
-        if("com.broadcastreceiver.EXAMPLE_ACTION".equals(intent.getAction())) {
-
-//            String receiveText = intent.getStringExtra("com.broadcastreceiver.EXTRA_TEXT");
-            String receviedText = intent.getStringExtra("com.broadcastreceiver.EXTRA_TEXT");
-            Toast.makeText(context, receviedText, Toast.LENGTH_SHORT).show();
-
-//            Log.i("Suman", " comes for connectivity options");
-//            Boolean noConnectivity = intent.getBooleanExtra(
-//                    ConnectivityManager.EXTRA_NO_CONNECTIVITY,
-//                    false
-//            );
-//
-//            if(noConnectivity) {
-//                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show();
-//            }
-//            else {
-//                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show();
-//            }
-        }
+        Toast.makeText(context, "EBR triggered First", Toast.LENGTH_SHORT).show();
+        
     }
 }
