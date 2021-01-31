464978712b2e395f973ce0ac23dafccc58ab40e4
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-02-01 02:01:21 +0600

    Retrofit Practice
    closes #34

--- RetrofitApplication/app/src/main/java/com/example/retrofitapplication/Post.java
+++ RetrofitApplication/app/src/main/java/com/example/retrofitapplication/Post.java
@@ -1,4 +1,11 @@
 package com.example.retrofitapplication;
 
-public class Post {
+public class Post  {
+
+    private int userId;
+    private int id;
+    private String title;
+    private String text;
+
+    // Need To Continue From Here
 }
