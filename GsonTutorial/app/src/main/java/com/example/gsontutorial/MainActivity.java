3ec8af03811df0cadabb5cdba05f75dc8e9d4e2d
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-02-01 01:45:09 +0600

    Part 4: Expose and Transient

--- GsonTutorial/app/src/main/java/com/example/gsontutorial/MainActivity.java
+++ GsonTutorial/app/src/main/java/com/example/gsontutorial/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
@@ -18,52 +19,74 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
-//        Address address = new Address("Germany", "Berlin");
-//        List<FamilyMember> family = new ArrayList<>();
-//
-//        family.add(new FamilyMember("wife",25));
-//        family.add(new FamilyMember("Daughter",7));
-//
-//        Employee employee = new Employee("Suman", 30, "suman@gmail.com",address, family);
-//        String json = gson.toJson(employee);
+        EmployeeSerializeDeserializePractice employee = new EmployeeSerializeDeserializePractice(
+            "Suman",
+            30,
+            "john@mail.com",
+            "asdfwew"
+        );
 
-        String json = "{\n" +
-                "  \"address\": {\n" +
-                "    \"city\": \"Berlin\",\n" +
-                "    \"country\": \"Germany\"\n" +
-                "  },\n" +
+        String jsonResult = gson.toJson(employee);
+
+        String result = "{\n" +
                 "  \"age\": 30,\n" +
-                "  \"family\": [\n" +
-                "    {\n" +
-                "      \"age\": 25,\n" +
-                "      \"role\": \"wife\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"age\": 7,\n" +
-                "      \"role\": \"Daughter\"\n" +
-                "    }\n" +
-                "  ],\n" +
-                "  \"first_name\": \"Suman\",\n" +
-                "  \"mail\": \"suman@gmail.com\"\n" +
+                "  \"firstName\": \"Suman\",\n" +
+                "  \"mail\": \"john@mail.com\"\n" +
                 "}";
 
-        Employee employee = gson.fromJson(json, Employee.class);
+        EmployeeSerializeDeserializePractice data = gson.fromJson(result, EmployeeSerializeDeserializePractice.class);
 
 
-        String familyJson = "[\n" +
-                "    {\n" +
-                "      \"age\": 25,\n" +
-                "      \"role\": \"wife\"\n" +
-                "    },\n" +
-                "    {\n" +
-                "      \"age\": 7,\n" +
-                "      \"role\": \"Daughter\"\n" +
-                "    }\n" +
-                "  ]";
 
-        Type familyType = new TypeToken < ArrayList<FamilyMember> > (){}.getType();
-        ArrayList<FamilyMember> familyMembers = gson.fromJson(familyJson, familyType );
+
+//        Gson gson = new Gson();
+//
+////        Address address = new Address("Germany", "Berlin");
+////        List<FamilyMember> family = new ArrayList<>();
+////
+////        family.add(new FamilyMember("wife",25));
+////        family.add(new FamilyMember("Daughter",7));
+////
+////        Employee employee = new Employee("Suman", 30, "suman@gmail.com",address, family);
+////        String json = gson.toJson(employee);
+//
+//        String json = "{\n" +
+//                "  \"address\": {\n" +
+//                "    \"city\": \"Berlin\",\n" +
+//                "    \"country\": \"Germany\"\n" +
+//                "  },\n" +
+//                "  \"age\": 30,\n" +
+//                "  \"family\": [\n" +
+//                "    {\n" +
+//                "      \"age\": 25,\n" +
+//                "      \"role\": \"wife\"\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "      \"age\": 7,\n" +
+//                "      \"role\": \"Daughter\"\n" +
+//                "    }\n" +
+//                "  ],\n" +
+//                "  \"first_name\": \"Suman\",\n" +
+//                "  \"mail\": \"suman@gmail.com\"\n" +
+//                "}";
+//
+//        Employee employee = gson.fromJson(json, Employee.class);
+//
+//
+//        String familyJson = "[\n" +
+//                "    {\n" +
+//                "      \"age\": 25,\n" +
+//                "      \"role\": \"wife\"\n" +
+//                "    },\n" +
+//                "    {\n" +
+//                "      \"age\": 7,\n" +
+//                "      \"role\": \"Daughter\"\n" +
+//                "    }\n" +
+//                "  ]";
+//
+//        Type familyType = new TypeToken < ArrayList<FamilyMember> > (){}.getType();
+//        ArrayList<FamilyMember> familyMembers = gson.fromJson(familyJson, familyType );
     }
 }
