2098ccd6cc46b3b349a680b287ec92cadda51d7f
Suman Bhadra <sumanbhadrasuman@gmail.com>
2021-02-25 20:01:08 +0600

    Retrofit Practice (#37)
    * Retrofit Practice
    
    closes #34
    
    * Part 1 - SIMPLE GET REQUES
    
    * 2. URL MANUPULATION
    
    * Post Request and Form URL ENCODED
    
    * Put, Patch & Delete
    
     Patch Need to Check Again

--- RetrofitApplication/app/src/main/java/com/example/retrofitapplication/Post.java
+++ RetrofitApplication/app/src/main/java/com/example/retrofitapplication/Post.java
@@ -1,4 +1,42 @@
 package com.example.retrofitapplication;
 
-public class Post {
+import com.google.gson.annotations.SerializedName;
+
+public class Post  {
+
+    private int userId;
+    private Integer id;
+    private String title;
+
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
+    @SerializedName("body")
+    private String text;
+
+//    public Post(int userId, int id, String title, String text) {
+//        this.userId = userId;
+//        this.id = id;
+//        this.title = title;
+//        this.text = text;
+//    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
